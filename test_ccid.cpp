f9885714a8b5eb1c6fc3fdb9b1f02bb89d7b8dc2
Szczepan Zalega <szczepan@nitrokey.com>
2023-05-09 18:45:26 +0200

    Reformat

--- test_ccid.cpp
+++ test_ccid.cpp
@@ -1,6 +1,6 @@
+#include "catch.hpp"
 #include <cstdint>
 #include <iostream>
-#include "catch.hpp"
 
 extern "C" {
 #include "../src/ccid.h"
@@ -10,18 +10,14 @@ extern "C" {
 }
 
 TLV data[] = {
-        {
-                .tag = 0x71,
-                .length = 6,
-                .type = 'S',
-                .v_str = "123456"
-        },
-        {
-                .tag = 0x71,
-                .length = 6,
-                .type = 'S',
-                .v_str = "789012"
-        },
+        {.tag = 0x71,
+         .length = 6,
+         .type = 'S',
+         .v_str = "123456"},
+        {.tag = 0x71,
+         .length = 6,
+         .type = 'S',
+         .v_str = "789012"},
 };
 
 
@@ -47,7 +43,7 @@ TEST_CASE("test tlv", "[Helper]") {
     print_buffer(buf, 64, "tlv res buffer");
     REQUIRE(buf[0] == data[0].tag);
     REQUIRE(buf[1] == data[0].length);
-    REQUIRE(buf[2+data[0].length] == data[1].tag);
-    REQUIRE(buf[2+data[0].length+1] == data[1].length);
+    REQUIRE(buf[2 + data[0].length] == data[1].tag);
+    REQUIRE(buf[2 + data[0].length + 1] == data[1].length);
     std::cout << buf;
 }
